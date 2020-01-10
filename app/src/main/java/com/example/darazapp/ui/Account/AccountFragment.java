f939bc5b7fbb98b6e6c7b2e438c86ba0f73c1027
prokash10 <prokasnjr10@gmail.com>
2020-01-10 10:17:17 +0545

    123

---
+++ app/src/main/java/com/example/darazapp/ui/Account/AccountFragment.java
@@ -0,0 +1,45 @@
+package com.example.darazapp.ui.Account;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.darazapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AccountFragment extends Fragment {
+    Button btnls;
+
+
+    public AccountFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        btnls = view.findViewById(R.id.btnSignInUp);
+
+        btnls.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ConnectActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
+
+}
